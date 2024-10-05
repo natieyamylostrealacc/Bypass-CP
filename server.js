f0e4a7790125a602a1dc589fcff310305e0b707d
natieyamylostrealacc <84051211+natieyamylostrealacc@users.noreply.github.com>
2024-10-05 08:19:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const users = new Set(); // To store connected usernames
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let chatMessages = []; // Store chat messages
+const activeUsers = {}; // Object to track active users and their messages
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('A user connected');
@@ -7,19 +22,27 @@ io.on('connection', (socket) => {
     socket.emit('chat history', chatMessages);
 
     socket.on('user joined', (username) => {
-        users.add(username);
-        socket.username = username; // Store username in socket
-        socket.broadcast.emit('user joined', username); // Notify others
+        socket.username = username; // Store the username in the socket
+        if (!activeUsers[username]) {
+            activeUsers[username] = []; // Initialize user's message history
+        }
+        socket.broadcast.emit('user joined', username);
     });
 
-    socket.on('chat message', ({ message }) => {
+    socket.on('chat message', (message) => {
         const chatMessage = { username: socket.username, message };
-        chatMessages.push(chatMessage);
+        chatMessages.push(chatMessage); // Store message in global history
+        activeUsers[socket.username].push(chatMessage); // Store in user's history
         io.emit('chat message', chatMessage);
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
-        users.delete(socket.username); // Remove user on disconnect
+        delete activeUsers[socket.username]; // Remove user on disconnect
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
