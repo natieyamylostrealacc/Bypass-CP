57d68e87cd7a54ad5b9c48bd0e4cb7dab1a96a55
natieyamylostrealacc <84051211+natieyamylostrealacc@users.noreply.github.com>
2024-10-05 08:04:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
+const users = new Set(); // To store connected usernames
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-// Array to hold chat messages
-let chatMessages = [];
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Route to serve index.html from the root directory
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Handle WebSocket connections
 io.on('connection', (socket) => {
     console.log('A user connected');
 
     // Send existing messages to the new user
     socket.emit('chat history', chatMessages);
 
-    socket.on('chat message', ({ username, message }) => {
-        const chatMessage = { username, message };
-        chatMessages.push(chatMessage); // Store message in the array
+    socket.on('user joined', (username) => {
+        users.add(username);
+        socket.username = username; // Store username in socket
+        socket.broadcast.emit('user joined', username); // Notify others
+    });
+
+    socket.on('chat message', ({ message }) => {
+        const chatMessage = { username: socket.username, message };
+        chatMessages.push(chatMessage);
         io.emit('chat message', chatMessage);
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
+        users.delete(socket.username); // Remove user on disconnect
     });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
 });
