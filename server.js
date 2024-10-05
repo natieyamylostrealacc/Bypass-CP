de5a45a7f7609f0768fbc8487419b21b21d1ad83
natieyamylostrealacc <84051211+natieyamylostrealacc@users.noreply.github.com>
2024-10-04 20:22:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,20 +7,28 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the 'public' directory (if you have other static files)
+// Array to hold chat messages
+let chatMessages = [];
+
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Update the route to serve index.html from the root directory
+// Route to serve index.html from the root directory
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Serve the main HTML file from root
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Handle WebSocket connections
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    // Send existing messages to the new user
+    socket.emit('chat history', chatMessages);
+
     socket.on('chat message', ({ username, message }) => {
-        io.emit('chat message', { username, message });
+        const chatMessage = { username, message };
+        chatMessages.push(chatMessage); // Store message in the array
+        io.emit('chat message', chatMessage);
     });
 
     socket.on('disconnect', () => {
