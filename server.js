f311da7d94c726865ce5f7cfc192171074798e29
natieyamylostrealacc <84051211+natieyamylostrealacc@users.noreply.github.com>
2024-10-04 20:14:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Serve static files from the 'public' directory (if you have other static files)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Update the route to serve index.html from the root directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html')); // Serve the main HTML file from root
+});
+
+// Handle WebSocket connections
 io.on('connection', (socket) => {
     console.log('A user connected');
 
